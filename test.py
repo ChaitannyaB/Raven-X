c6561d8fcbe43e26e8e14f36bea51b393094fc14
kinglotusss <122764087+kinglotusss@users.noreply.github.com>
2023-05-04 20:28:18 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-complete_list=["hello", "hey", "hi","how are you",
-               "shut down","close process","terminate",
-               "google","search for","look for",
-               "take a note","open note","notepad","write","remember","note",
-                "what do i have","schedule","plans","what am i doing","am i busy","am i free","what i have"] 
-
-if 'hello' in complete_list:
-    print(1)
